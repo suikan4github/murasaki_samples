b601b6d7a3742c293f0d1fae12196f543e898208
takemasa <suikan@users.osdn.me>
2019-02-11 11:19:46 +0900

    Add push butten interrupt and I2C search.

--- nucleo-f746-sample/Inc/stm32f7xx_it.h
+++ nucleo-f746-sample/Inc/stm32f7xx_it.h
@@ -73,6 +73,7 @@ void DMA1_Stream3_IRQHandler(void);
 void I2C1_EV_IRQHandler(void);
 void I2C1_ER_IRQHandler(void);
 void USART3_IRQHandler(void);
+void EXTI15_10_IRQHandler(void);
 void TIM8_TRG_COM_TIM14_IRQHandler(void);
 void DMA2_Stream0_IRQHandler(void);
 void DMA2_Stream3_IRQHandler(void);
